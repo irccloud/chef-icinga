950cc6c04d4838662c75a9532c8528f4c3e57cab
Russ Garrett <russ@garrett.co.uk>
2013-11-25 15:53:02 +0000

    Default hostgroups

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,7 @@ default[:icinga][:node_query] = 'chef_environment:*'
 
 default[:icinga][:apache_port] = 80
 default[:icinga][:url] = 'icinga'
+default[:icinga][:default_hostgroups] = ['servers']
 default[:icinga][:url_aliases] = []
 default[:icinga][:path] = ''
 default[:icinga][:ssl] = false
